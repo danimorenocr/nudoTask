fdee8144f76fba9a6e02d89474eaf3350517797e
Dani Moreno <danimorenocr@gmail.com>
2025-01-07 16:03:29 -0500

    Estructura Base

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+   function App() {
+       return (
+           <div>
+               <h1>Bienvenido a Nudo Task</h1>
+           </div>
+       );
+   }
+
+   export default App;
